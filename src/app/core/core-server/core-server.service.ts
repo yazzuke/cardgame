ee793f514e49bf38fa4f0f2c0c4315b8210a572c
David Hernandez <davidfelipehernandez1@gmail.com>
2024-02-04 17:02:17 -0500

    add http response to apis

--- src/app/core/core-server/core-server.service.ts
+++ src/app/core/core-server/core-server.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environments } from '../../../environments/environments';
 import { firstValueFrom } from 'rxjs';
@@ -13,30 +13,47 @@ export class CoreServerService {
   constructor(private http: HttpClient) {}
 
   // Users API
-  getUsers(): Promise<IUser[]> {
-    return firstValueFrom(this.http.get<IUser[]>(`${this.baseUri}/users/all`));
-  }
-
-  createUser(username: string): Promise<IUser> {
+  getUsers(): Promise<HttpResponse<IUser[]>> {
     return firstValueFrom(
-      this.http.post<IUser>(`${this.baseUri}/users/register`, {
-        username: username,
+      this.http.get<IUser[]>(`${this.baseUri}/users/all`, {
+        observe: 'response',
       })
     );
   }
 
-  // Scoreboard API
-  getScoreboard(): Promise<IScoreboard[]> {
+  createUser(username: string): Promise<HttpResponse<IUser>> {
     return firstValueFrom(
-      this.http.get<IScoreboard[]>(`${this.baseUri}/scoreboard/getscoreboard`)
+      this.http.post<IUser>(
+        `${this.baseUri}/users/register`,
+        {
+          username: username,
+        },
+        {
+          observe: 'response',
+        }
+      )
     );
   }
 
-  createUserScore(userScoreboard: IScoreboard): Promise<IScoreboard> {
+  // Scoreboard API
+  getScoreboard(): Promise<HttpResponse<IScoreboard[]>> {
+    return firstValueFrom(
+      this.http.get<IScoreboard[]>(`${this.baseUri}/scoreboard/getscoreboard`, {
+        observe: 'response',
+      })
+    );
+  }
+
+  createUserScore(
+    userScoreboard: IScoreboard
+  ): Promise<HttpResponse<IScoreboard>> {
     return firstValueFrom(
       this.http.post<IScoreboard>(
         `${this.baseUri}/scoreboard/postscoreboard`,
-        userScoreboard
+        userScoreboard,
+        {
+          observe: 'response',
+        }
       )
     );
   }
